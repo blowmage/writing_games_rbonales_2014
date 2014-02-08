8ec4d6571097c257e9e84ce66570d3fc1f62c8f9
Mike Moore <mike@blowmage.com>
2014-02-08 13:52:26 -0800

    Can we drop our sprite in?
    Turns out we can, but it behaves strange near the edge of the map.
    The view/camera is based on keys pressed, not position of the sprite.
    Let's fix that.

--- parallax/plx.rb
+++ parallax/plx.rb
@@ -1,10 +1,59 @@
 require "gosu"
 
+class Sprite
+  def initialize window
+    @window = window
+    # image
+    @width = @height = 160
+    @idle = Gosu::Image.load_tiles @window,
+                                   "player_160x160_idle.png",
+                                   @width, @height, true
+    @move = Gosu::Image.load_tiles @window,
+                                   "player_160x160_move.png",
+                                   @width, @height, true
+    # center image
+    @x = @window.width/2  - @width/2
+    @y = @window.height/2 - @height/2
+    # direction and movement
+    @direction = :right
+    @frame = 0
+    @moving = false
+  end
+
+  def update
+    @frame += 1
+    @moving = false
+    if @window.button_down? Gosu::KbLeft
+      @direction = :left
+      @moving = true
+      @x += -5
+    elsif @window.button_down? Gosu::KbRight
+      @direction = :right
+      @moving = true
+      @x += 5
+    end
+  end
+
+  def draw
+    # @move and @idle are the same size,
+    # so we can use the same frame calc.
+    f = @frame % @idle.size
+    image = @moving ? @move[f] : @idle[f]
+    if @direction == :right
+      image.draw @x, @y, 1
+    else
+      image.draw @x + @width, @y, 1, -1, 1
+    end
+  end
+
+end
+
 class PlxGame < Gosu::Window
   def initialize width=800, height=600, fullscreen=false
     super
     @foreground = Gosu::Image.new(self, "foreground.png", false)
     @background = Gosu::Image.new(self, "background.png", false)
+    @sprite = Sprite.new self
     @movement = 8
     @x = @y = 0
   end
@@ -29,6 +78,7 @@ class PlxGame < Gosu::Window
     move_right if button_down? Gosu::KbRight
     move_left  if button_down? Gosu::KbLeft
     close      if button_down? Gosu::KbEscape
+    @sprite.update
   end
 
   def plx_ratio
@@ -49,6 +99,7 @@ class PlxGame < Gosu::Window
     end
     translate *cam_coords do
       @foreground.draw 0, 0, -1
+      @sprite.draw
     end
   end
 end
